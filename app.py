307b3fb44de6c451ecf949d22e806bed238a5469
Syver <106058254+Syver-b@users.noreply.github.com>
2026-04-20 20:40:02 +0200

    app.route
    Lagt til app.route for alle endepunkt med metoden som blir etterspurt i oppgaven

--- app.py
+++ app.py
@@ -24,17 +24,25 @@ def get_grade(marks):
 # AAA Database innhold her AAA
 
 # Endpoint 1: GET all students
+@app.route('/api/students', methods=['GET'])
 
 # Endpoint 2: GET student by ID
+@app.route('/api/students/{id}', methods=['GET'])
 
 # Endpoint 3: POST new student
+@app.route('/api/students', methods=['POST'])
 
 # Endpoint 4: PUT update student
+@app.route('/api/students/{id}', methods=['PUT'])
 
 # Endpoint 5: DELETE student
+@app.route('/api/students/{id}', methods=['DELETE'])
 
 # Endpoint 6: POST calculate grade for student
+@app.route('/api/students/calculate-grades', methods=['POST'])
 
 # Endpoint 7: GET report of courses and average grades
+@app.route('/api/students/report', methods=['GET'])
 
 # Endpoint 8: GET health check
+@app.route('/health', methods=['GET'])
